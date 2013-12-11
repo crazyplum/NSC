9b4f0cf68a58262fea0be4f1f5d47d883044a4de
crazyplum <crazyplum24@gmail.com>
2013-12-11 15:36:16 +0800

    finish first stage activity layout adjustment

--- MTHighlightProject/MTHighlight/src/main/java/com/plum/mthighlight/activity/FirstStageActivity.java
+++ MTHighlightProject/MTHighlight/src/main/java/com/plum/mthighlight/activity/FirstStageActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.util.Pair;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
@@ -17,6 +18,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.plum.mthighlight.R;
+import com.plum.mthighlight.utility.TestConditionOrder;
 
 import org.w3c.dom.Text;
 
@@ -26,21 +28,25 @@ public class FirstStageActivity extends ActionBarActivity {
 
     int testcase;
     int current_state;
+    PlaceholderFragment mFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-
         Intent intent = getIntent();
         testcase = intent.getIntExtra("testcase", -1);
         current_state = intent.getIntExtra("current_state", -1);
+        mFragment = new PlaceholderFragment(testcase);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, mFragment)
+                    .commit();
+        }
+
+
     }
 
 
@@ -59,6 +65,7 @@ public class FirstStageActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         switch (item.getItemId()) {
             case R.id.okay:
+                mFragment.getNewSentence();
                 return true;
         }
         return super.onOptionsItemSelected(item);
@@ -73,10 +80,16 @@ public class FirstStageActivity extends ActionBarActivity {
         LinearLayout row2;
         LinearLayout row3;
         LinearLayout row4;
-        ArrayList<TextView> textviewList;
-        int index = 0;
+        LinearLayout row5;
+        LinearLayout row6;
 
-        public PlaceholderFragment() {
+        ArrayList<TextView> textviewList;
+        int index;
+        int testcase;
+
+        public PlaceholderFragment(int t) {
+            testcase = t;
+            index = 0;
         }
 
         @Override
@@ -88,18 +101,20 @@ public class FirstStageActivity extends ActionBarActivity {
             row2 = (LinearLayout) rootView.findViewById(R.id.row2);
             row3 = (LinearLayout) rootView.findViewById(R.id.row3);
             row4 = (LinearLayout) rootView.findViewById(R.id.row4);
+            row5 = (LinearLayout) rootView.findViewById(R.id.row5);
+            row6 = (LinearLayout) rootView.findViewById(R.id.row6);
 
-            generateSentence();
+            generateSentence(TestConditionOrder.ORDER.get(testcase)[index + (testcase%6)*3]);
 
             return rootView;
         }
 
-        private TextView generateTextView(String word, String group){
+        private TextView generateTextView(String word){
             TextView tv = new TextView(getActivity());
             tv.setText(word);
             tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
-            tv.setTag(R.id.group, group);
             tv.setTag(R.id.highlight, Boolean.FALSE);
+            tv.setTag(R.id.group, "0");
             tv.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -118,45 +133,77 @@ public class FirstStageActivity extends ActionBarActivity {
                     }
                 }
             });
+
             return tv;
         }
 
         private void generateSentence(int k){
+
             String sentence = IntroActivity.oriSentences.get(k);
             ArrayList<Pair<String, String>> pairs = IntroActivity.sentencesMap.get(k);
             textviewList = new ArrayList<TextView>();
 
+            String words[] = sentence.split(" ");
             int count = 0;
-            for(int j = 0; j < pairs.size(); j++){
-                Pair<String, String> p = pairs.get(j);
-                TextView tv, spacetv;
-                String str = p.first;
-                String words[] = str.split(" ");
+            TextView tv, sv;
 
-                for(int i = 0; i < words.length; i++){
-                    tv = generateTextView(words[i], Integer.toString(j));
-                    spacetv = generateTextView(" ", Integer.toString(j));
-                    if(count + words[i].length() < 33){
-                        row1.addView(tv);
-                        row1.addView(spacetv);
-                        count += words[i].length() + 1;
-                    }else if(count + words[i].length() < 66){
-                        row2.addView(tv);
-                        row2.addView(spacetv);
-                        count += words[i].length() + 1;
-                    }else if(count + words[i].length() < 99){
-                        row3.addView(tv);
-                        row3.addView(spacetv);
-                        count += words[i].length() + 1;
-                    }else{
-                        row4.addView(tv);
-                        row4.addView(spacetv);
-                        count += words[i].length() + 1;
+            for(int i = 0; i < words.length; i++){
+                tv = generateTextView(words[i]);
+                sv = generateTextView(" ");
+
+                if(count + words[i].length() <= 32){
+                    row1.addView(tv);
+                    row1.addView(sv);
+                    count += words[i].length() + 1;
+                }else if(count + words[i].length() <= 59){
+                    row2.addView(tv);
+                    row2.addView(sv);
+                    count += words[i].length() + 1;
+                }else if(count + words[i].length() <= 89){
+                    row3.addView(tv);
+                    row3.addView(sv);
+                    count += words[i].length() + 1;
+                }else if(count + words[i].length() <= 119){
+                    row4.addView(tv);
+                    row4.addView(sv);
+                    count += words[i].length() + 1;
+                }else if(count + words[i].length() <= 150){
+                    row5.addView(tv);
+                    row5.addView(sv);
+                    count += words[i].length() + 1;
+                }else{
+                    row6.addView(tv);
+                    row6.addView(sv);
+                }
+                textviewList.add(tv);
+            }
+
+            for(int i = 0; i < pairs.size(); i++){
+                Pair<String, String> p = pairs.get(i);
+                String str = p.first;
+                String w[]  = str.split(" ");
+
+                for(int j = 0; j < w.length; j++){
+                    for(TextView t: textviewList){
+                        String word = (String)t.getText();
+                        word = word.replaceAll("[^a-zA-z0-9]", "");
+                        if(word.equals(w[j])){
+                            t.setTag(R.id.group, Integer.toString(i+1));
+                        }
                     }
-                    textviewList.add(tv);
-                    textviewList.add(spacetv);
                 }
             }
+        }
+
+        public void getNewSentence(){
+            index++;
+            row1.removeAllViews();
+            row2.removeAllViews();
+            row3.removeAllViews();
+            row4.removeAllViews();
+            row5.removeAllViews();
+            row6.removeAllViews();
+            generateSentence(TestConditionOrder.ORDER.get(testcase)[index + (testcase%6)*3]);
         }
     }
 
