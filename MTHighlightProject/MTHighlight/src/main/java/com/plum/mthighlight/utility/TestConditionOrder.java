fe8894506f8ec21da4a8ac9096af5240a7bad37d
crazyplum <crazyplum24@gmail.com>
2013-12-11 15:37:16 +0800

    finish test condition order

--- MTHighlightProject/MTHighlight/src/main/java/com/plum/mthighlight/utility/TestConditionOrder.java
+++ MTHighlightProject/MTHighlight/src/main/java/com/plum/mthighlight/utility/TestConditionOrder.java
@@ -16,6 +16,7 @@ public class TestConditionOrder {
 
 
     public static List<List<Class<?>>> TESTCASE = new ArrayList<List<Class<?>>>();
+    public static List<int[]> ORDER = new ArrayList<int[]>();
     static{
         TESTCASE.add(Arrays.<Class<?>>asList(FirstStageActivity.class, SecondStageActivity.class, ThirdStageActivity.class));
         TESTCASE.add(Arrays.<Class<?>>asList(FirstStageActivity.class, ThirdStageActivity.class, SecondStageActivity.class));
@@ -23,7 +24,31 @@ public class TestConditionOrder {
         TESTCASE.add(Arrays.<Class<?>>asList(SecondStageActivity.class, ThirdStageActivity.class, FirstStageActivity.class));
         TESTCASE.add(Arrays.<Class<?>>asList(ThirdStageActivity.class, FirstStageActivity.class, SecondStageActivity.class));
         TESTCASE.add(Arrays.<Class<?>>asList(ThirdStageActivity.class, SecondStageActivity.class, FirstStageActivity.class));
+
+        ORDER.add(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8});
+        ORDER.add(new int[]{0, 2, 1, 3, 4, 5, 6, 7, 8});
+        ORDER.add(new int[]{0, 1, 2, 3, 4, 5, 6, 8, 7});
+        ORDER.add(new int[]{0, 2, 1, 3, 5, 4, 6, 7, 8});
+        ORDER.add(new int[]{0, 1, 2, 3, 5, 4, 6, 8, 7});
+        ORDER.add(new int[]{0, 2, 1, 3, 5, 4, 6, 8, 7});
+
+        ORDER.add(new int[]{7, 6, 8, 1, 0, 2, 4, 3, 5});
+        ORDER.add(new int[]{7, 8, 6, 1, 0, 2, 4, 3, 5});
+        ORDER.add(new int[]{7, 6, 8, 1, 0, 2, 4, 5, 3});
+        ORDER.add(new int[]{7, 8, 6, 1, 2, 0, 4, 3, 5});
+        ORDER.add(new int[]{7, 6, 8, 1, 2, 0, 4, 5, 3});
+        ORDER.add(new int[]{7, 8, 6, 1, 2, 0, 4, 5, 3});
+
+        ORDER.add(new int[]{5, 3, 4, 8, 6, 7, 2, 1, 0});
+        ORDER.add(new int[]{5, 4, 3, 8, 6, 7, 2, 1, 0});
+        ORDER.add(new int[]{5, 3, 4, 8, 6, 7, 2, 0, 1});
+        ORDER.add(new int[]{5, 4, 3, 8, 7, 6, 2, 0, 1});
+        ORDER.add(new int[]{5, 3, 4, 8, 7, 6, 2, 0, 1});
+        ORDER.add(new int[]{5, 4, 3, 8, 7, 6, 2, 1, 0});
+
+
     }
+
 
 
 
